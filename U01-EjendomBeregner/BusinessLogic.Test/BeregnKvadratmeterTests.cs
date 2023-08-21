501cf6a3141b69d63b457df5c8d8496bed97ab9a
KBR <kbr@ucl.dk>
2023-08-21 10:17:41 +0200

    xUnit poroject added. Kvadratmeter_Sum_Stemmer_Med_Lejemaalsum addded.

---
+++ U01-EjendomBeregner/BusinessLogic.Test/BeregnKvadratmeterTests.cs
@@ -0,0 +1,11 @@
+namespace BusinessLogic.Test
+{
+    public class BeregnKvadratmeterTests
+    {
+        [Fact]
+        public void Kvadratmeter_Sum_Stemmer_Med_Lejemaalsum()
+        {
+
+        }
+    }
+}
