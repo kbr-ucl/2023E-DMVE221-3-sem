d83de83d6f79b821a46ca0a5b02e7e0325d98f1c
KBR <kbr@ucl.dk>
2023-08-23 19:18:37 +0200

    BusinessLogic done

--- U01-EjendomBeregner/EjendomBeregner.BusinessLogic/EjendomBeregner.BusinessLogic.cs
+++ U01-EjendomBeregner/EjendomBeregner.BusinessLogic/EjendomBeregner.BusinessLogic.cs
@@ -1,37 +1,73 @@
-﻿namespace EjendomBeregner.BusinessLogic
+﻿using EjendomBeregner.Domain;
+using EjendomBeregner.Infrastructure;
+
+namespace EjendomBeregner.BusinessLogic
 {
+    /// <summary>
+    ///     Application service der beregner ejendommens kvadratmeter.
+    /// </summary>
     public class EjendomBeregnerService
     {
-        /// <summary>
-        ///     Beregner ejendommens kvadratmeter ud fra ejendommens lejelmål.
-        ///     Lejemål er i en kommasepareret tekstfil. Formatet af filen er:
-        ///     "lejlighednummer", "kvadratmeter", "antal rum"
-        ///     lejlighednummer: int
-        ///     kvadratmeter: double
-        ///     antal rum: double
-        /// </summary>
-        /// <param name="lejemaalDataFilename"></param>
-        /// <returns></returns>
-        public double BeregnKvadratmeter(string lejemaalDataFilename)
+        private readonly ILejemaalRepository _lejemaalRepository;
+
+        public EjendomBeregnerService(ILejemaalRepository lejemaalRepository)
         {
-            var lejemaalene = File.ReadAllLines(lejemaalDataFilename);
-            var kvadratmeter = 0.0;
-
-
-            foreach (var lejemaal in lejemaalene)
-            {
-                var lejemaalParts = lejemaal.Split(',');
-                double lejemaalKvadratmeter;
-                double.TryParse(RemoveQuotes(lejemaalParts[1]), out lejemaalKvadratmeter);
-                kvadratmeter += lejemaalKvadratmeter;
-            }
-
-            return kvadratmeter;
+            _lejemaalRepository = lejemaalRepository;
         }
 
-        private string RemoveQuotes(string lejemaalPart)
+        /// <summary>
+        ///     Beregner ejendommens kvadratmeter ud fra ejendommens lejelmål.
+        /// </summary>
+        /// <returns>Ejendommens kvadratmeter</returns>
+        public double BeregnKvadratmeter()
         {
-            return lejemaalPart.Replace('"', ' ').Trim();
+            var lejemaals = _lejemaalRepository.HentLejemaal();
+            double kvadratmeter = 0;
+            foreach (var lejemaal in lejemaals) kvadratmeter += lejemaal.Kvadratmeter;
+            return kvadratmeter;
+        }
+    }
+}
+
+namespace EjendomBeregner.Domain
+{
+    /// <summary>
+    ///     Domain model for lejemål
+    /// </summary>
+    public class Lejemaal
+    {
+        public int Lejlighednummer { get; set; }
+        public double Kvadratmeter { get; set; }
+        public double Rum { get; set; }
+    }
+}
+
+namespace EjendomBeregner.Infrastructure
+{
+    /// <summary>
+    ///     Repository interface for lejemål
+    /// </summary>
+    public interface ILejemaalRepository
+    {
+        /// <summary>
+        ///     Indlæser ejendommens lejelmål.
+        /// </summary>
+        /// <returns>Liste af ejendommens lejemål</returns>
+        IEnumerable<Lejemaal> HentLejemaal();
+    }
+
+    /// <summary>
+    ///     Implementation af ILejemaalRepository der indlæser fake.
+    ///     <summary></summary>
+    public class LejemaalFakeRepository : ILejemaalRepository
+    {
+        public IEnumerable<Lejemaal> HentLejemaal()
+        {
+            return new List<Lejemaal>(new[] {
+            new Lejemaal {Lejlighednummer = 1, Kvadratmeter = 100, Rum = 4},
+            new Lejemaal {Lejlighednummer = 2, Kvadratmeter = 80.4, Rum = 3},
+            new Lejemaal {Lejlighednummer = 3, Kvadratmeter = 60, Rum = 2.5}
+            });
         }
     }
 }
