fa9c9199bb29873ad5f5c1b39c1aa42813dd1dde
KBR <kbr@ucl.dk>
2023-11-09 17:45:56 +0100

    API ready

--- U-12-FrontEndBackend/DomainCentricDemo.Api/Program.cs
+++ U-12-FrontEndBackend/DomainCentricDemo.Api/Program.cs
@@ -10,6 +10,7 @@ using System.Diagnostics;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
